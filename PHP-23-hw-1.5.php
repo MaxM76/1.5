123fe497037169461d6e86851c2c7d529cb2bbcc
MaxM76 <38570363+MaxM76@users.noreply.github.com>
2018-07-11 18:03:53 +0300

    Add files via upload

--- PHP-23-hw-1.5.php
+++ PHP-23-hw-1.5.php
@@ -1,3 +1,30 @@
+<?php
+$link = 'http://api.openweathermap.org/data/2.5/weather';
+$cityId = '524901';
+$appId = '5339eb2c19539b482dacee95c4b20479';
+$request = $link . '?id=' . $cityId . '&APPID=' . $appId;
+
+$weatherJson = file_get_contents ($request);
+if ($weatherJson === false) {
+    exit('Не удалось получить данные');
+}
+
+$curWeather = json_decode ($weatherJson, true);
+if ($curWeather === null) {
+    exit('Ошибка декодирования');
+}
+
+$iconURI = 'http://openweathermap.org/img/w/' . $curWeather["weather"][0]["icon"] . '.png';
+$weatherIconTag = '<img src="' . $iconURI . '" alt="">';
+$weatherCharacter = (!empty($curWeather["weather"][0]["main"])) ? $curWeather["weather"][0]["main"] : 'не удалось получить данные';
+$weatherDiscr = (!empty($curWeather["weather"][0]["description"])) ? $curWeather["weather"][0]["description"] : 'не удалось получить данные';
+$weatherTemp = (!empty($curWeather["main"]["temp"])) ? $curWeather["main"]["temp"] - 273.15 : 'нет данных';
+$weatherPressure = (!empty($curWeather["main"]["pressure"])) ? round($curWeather["main"]["pressure"] / 1.333, 1) : 'нет данных';
+$weatherHumidity = (!empty($curWeather["main"]["humidity"])) ? $curWeather["main"]["humidity"] : 'нет данных';
+$weatherMinTemp = (!empty($curWeather["main"]["temp_min"])) ? $curWeather["main"]["temp_min"] - 273.15 : 'нет данных';
+$weatherMaxTemp = (!empty($curWeather["main"]["temp_max"])) ? $curWeather["main"]["temp_max"] - 273.15 : 'нет данных';
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
   <head>
@@ -12,23 +39,14 @@
 
 
 <?php
-    $city_id = "?id=524901";
-    $appid = "&APPID=5339eb2c19539b482dacee95c4b20479";
-    $request = "http://api.openweathermap.org/data/2.5/weather" . $city_id . $appid;
-
-    $weather_json = file_get_contents ($request);
-
-    $cur_weather = json_decode ($weather_json, true);
-    $icon_uri = "http://openweathermap.org/img/w/" . $cur_weather["weather"][0]["icon"] . ".png";
-
-    echo "<h2>Погода в Москве" .  "<img src=\"" . $icon_uri . "\" alt=\"\"></h2>";
-    echo "<p>Общий характер погоды: " . $cur_weather["weather"][0]["main"] . "</p>";
-    echo "<p>Описание погоды: " . $cur_weather["weather"][0]["description"] . "</p>";
-    echo "<p>Температура: " .  ($cur_weather["main"]["temp"] - 273.15) . " градусов Цельсия</p>";
-    echo "<p>Атмосферное давление: " . round($cur_weather["main"]["pressure"] / 1.333, 1) . " мм рт.ст.</p>";
-    echo "<p>Влажность: " . $cur_weather["main"]["humidity"] . "%</p>";
-    echo "<p>Минимальная температура: " . ($cur_weather["main"]["temp_min"] - 273.15) . " градусов Цельсия</p>";
-    echo "<p>Максимальная температура: " . ($cur_weather["main"]["temp_max"] - 273.15) . " градусов Цельсия</p>";
+    echo "<h2>Погода в Москве$weatherIconTag</h2>";
+    echo "<p>Общий характер погоды: $weatherCharacter</p>";
+    echo "<p>Описание погоды: $weatherDiscr</p>";
+    echo "<p>Температура: $weatherTemp градусов Цельсия</p>";
+    echo "<p>Атмосферное давление: $weatherPressure мм рт.ст.</p>";
+    echo "<p>Влажность: $weatherHumidity%</p>";
+    echo "<p>Минимальная температура: $weatherMinTemp градусов Цельсия</p>";
+    echo "<p>Максимальная температура: $weatherMaxTemp градусов Цельсия</p>";
 ?>
 
   </body>
